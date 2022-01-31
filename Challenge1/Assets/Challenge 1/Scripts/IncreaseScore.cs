628cd59a662358603bbfc812ac6d586f29aea25e
Pyroghost0 <ckahn@mail.bradley.edu>
2022-01-30 19:47:10 -0600

    Finish Challenge 1

---
+++ Challenge1/Assets/Challenge 1/Scripts/IncreaseScore.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Caleb Kahn
+ * Assignment 2
+ * Increases score when player hits trigger zone
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IncreaseScore : MonoBehaviour
+{
+    private bool triggered = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !triggered)
+        {
+            triggered = true;
+            ScoreWinLose.score++;
+        }
+    }
+}
---
+++ Challenge1/Assets/Challenge 1/Scripts/Spin.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Caleb Kahn
+ * Assignment 2
+ * spins object
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+
+    public float turnSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, turnSpeed * Time.deltaTime);
+    }
+}
