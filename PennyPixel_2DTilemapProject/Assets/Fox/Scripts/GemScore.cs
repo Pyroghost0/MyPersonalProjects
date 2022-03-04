6e0a4b85cb1e4e30be17d7862be16e49f0aa6924
Pyroghost0 <89740009+Pyroghost0@users.noreply.github.com>
2022-03-04 16:04:20 -0600

    Finished Assignment 5A

---
+++ PennyPixel_2DTilemapProject/Assets/Fox/Scripts/GemScore.cs
@@ -0,0 +1,26 @@
+﻿/*Caleb Kahn
+ * Assignment 5A
+ * Updates score when a gem is collected
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemScore : MonoBehaviour
+{
+
+    private ScoreAndEndManager sm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sm = GameObject.FindGameObjectWithTag("ScoreAndEndManager").GetComponent<ScoreAndEndManager>();
+    }
+
+    void OnTriggerEnter2D(Collider2D theCollider)
+    {
+        if (theCollider.CompareTag("Player")) {
+            sm.score++;
+        }
+    }
+}
---
+++ PennyPixel_2DTilemapProject/Assets/Fox/Scripts/ScoreAndEndManager.cs
@@ -0,0 +1,41 @@
+﻿/*Caleb Kahn
+ * Assignment 5A
+ * Displays correct score and winning text
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreAndEndManager : MonoBehaviour
+{
+
+    public int score = 0;
+    private GameObject player;
+    private bool finished = false;
+
+    public Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!finished) {
+            scoreText.text = "Score: " + score;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D theCollider)
+    {
+        if (theCollider.CompareTag("Player") && score == 10) 
+        {
+            finished = true;
+            scoreText.text = "You win!";
+        }
+    }
+}
