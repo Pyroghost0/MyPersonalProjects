d6d79e1d665eea9deca18a1e7cbf2a8dd06b4ef0
Pyroghost0 <89740009+Pyroghost0@users.noreply.github.com>
2022-04-11 21:47:19 -0500

    Finished Assignment 8

---
+++ Prototype5/Assets/Scripts/DifficultyButton.cs
@@ -0,0 +1,29 @@
+﻿/* Caleb Kahn
+ * Assignment 8
+ * Set difficulty via button
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyButton : MonoBehaviour
+{
+    private Button button;
+    private GameManager gameManager;
+    public int difficulty;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+        button.onClick.AddListener(SetDifficulty);
+    }
+
+    // Update is called once per frame
+    void SetDifficulty()
+    {
+        gameManager.StartGame(difficulty);
+    }
+}
---
+++ Prototype5/Assets/Scripts/Target.cs
@@ -0,0 +1,65 @@
+﻿/* Caleb Kahn
+ * Assignment 8
+ * Acts as a target that can be clicked on for points
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody targetRB;
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float maxTorque = 10;
+    private float xRange = 4;
+    private float ySpawnPos = -6;
+
+    private GameManager gameManager;
+    public int pointValue;
+    public ParticleSystem explosionParticle;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRB = GetComponent<Rigidbody>();
+        targetRB.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRB.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+        transform.position = RandomSpawnPos();
+        gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+    }
+
+    private Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+    }
+
+    private float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    private Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    private void OnMouseDown()
+    {
+        if(gameManager.isGameActive)
+        {
+            gameManager.UpdateScore(pointValue);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
+        Destroy(gameObject);
+    }
+}
